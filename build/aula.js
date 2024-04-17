e196d06ff8ddf3dc3d8be38156160f5ef1690e6c
Fellipe Castro <fellipecastro27d@gmail.com>
2024-04-17 17:26:54 -0300

    aula 04 finalizada

--- build/aula.js
+++ build/aula.js
@@ -1,12 +1,6 @@
 "use strict";
-class Curso {
-    canal = null;
-    curso = null;
-    constructor(canal, curso) {
-        this.canal = canal;
-        this.curso = curso;
-    }
-}
-let c1 = new Curso('CFB Cursos', 'Typescript');
-console.log(c1.canal);
-console.log(c1.curso);
+let nome;
+nome = 'Fellipe';
+nome = 'João';
+let valor;
+valor = 1;
--- src/aula.ts
+++ src/aula.ts
@@ -1,13 +1,21 @@
-class Curso{
-    canal = null
-    curso = null
-    constructor(canal: any, curso: any) {
-        this.canal = canal
-        this.curso = curso
-    }
-}
+// class Curso{
+//     canal = null
+//     curso = null
+//     constructor(canal: any, curso: any) {
+//         this.canal = canal
+//         this.curso = curso
+//     }
+// }
 
-let c1 = new Curso('CFB Cursos', 'Typescript')
+// let c1 = new Curso('CFB Cursos', 'Typescript')
 
-console.log(c1.canal)
-console.log(c1.curso)
+// console.log(c1.canal)
+// console.log(c1.curso)
+
+let nome: string
+nome = 'Fellipe'
+nome = 'João'
+
+let valor: number
+//valor = 'string' /* vai dar erro porque não é aceito outro tipo */
+valor = 1
