2ae39f0089d8630a9f5f8d54bfcee25946220c4b
Fellipe Castro <fellipecastro27d@gmail.com>
2024-04-20 14:28:29 -0300

    aula 10 finalizada

--- build/aula.js
+++ build/aula.js
@@ -1,36 +1,9 @@
 "use strict";
-var dias;
-(function (dias) {
-    dias[dias["domingo"] = 0] = "domingo";
-    dias[dias["segunda"] = 1] = "segunda";
-    dias[dias["terca"] = 2] = "terca";
-    dias[dias["quarta"] = 3] = "quarta";
-    dias[dias["quinta"] = 4] = "quinta";
-    dias[dias["sexta"] = 5] = "sexta";
-    dias[dias["sabado"] = 6] = "sabado";
-})(dias || (dias = {}));
-console.log(dias.domingo);
-console.log(dias['segunda']);
-console.log(dias[3]);
-const dia = new Date();
-console.log(dia.getDay());
-console.log(dias[dia.getDay()]);
-var cores;
-(function (cores) {
-    cores["branco"] = "#fff";
-    cores["preto"] = "#000";
-    cores["vermelho"] = "#f00";
-    cores["verde"] = "#0f0";
-    cores["azul"] = "#00f";
-})(cores || (cores = {}));
-console.log(cores.branco);
-console.log(cores['verde']);
-var tipoUsuario;
-(function (tipoUsuario) {
-    tipoUsuario[tipoUsuario["user"] = 0] = "user";
-    tipoUsuario[tipoUsuario["admin"] = 1] = "admin";
-    tipoUsuario[tipoUsuario["super"] = 2] = "super";
-})(tipoUsuario || (tipoUsuario = {}));
-console.log(tipoUsuario.super);
-const tipo = 1;
-const tipo2 = tipoUsuario.admin;
+let nome;
+nome = null;
+console.log(nome);
+let nome2;
+console.log(nome2);
+let nome3 = 1;
+let num = nome3;
+console.log(nome3);
--- src/aula.ts
+++ src/aula.ts
@@ -1,43 +1,14 @@
-// enum numerico
-enum dias {
-    domingo = 0,
-    segunda = 1,
-    terca = 2,
-    quarta = 3,
-    quinta = 4,
-    sexta = 5,
-    sabado = 6,
-}
+// null = nulo != vazio
+let nome: string|null
+nome = null
+console.log(nome)
 
-console.log(dias.domingo)
-console.log(dias['segunda'])
-console.log(dias[3])
+// undefined = indefinido/não tem valor
+let nome2
+console.log(nome2)
 
-const dia = new Date()
-console.log(dia.getDay())
-
-console.log(dias[dia.getDay()])
-
-// enum textual
-enum cores {
-    branco = '#fff',
-    preto = '#000',
-    vermelho = '#f00',
-    verde = '#0f0',
-    azul = '#00f',
-}
-
-console.log(cores.branco)
-console.log(cores['verde'])
-
-enum tipoUsuario {
-    user,
-    admin,
-    super
-}
-
-console.log(tipoUsuario.super)
-
-// usando enum como tipo
-const tipo: tipoUsuario = 1
-const tipo2: tipoUsuario = tipoUsuario.admin
+// unknown = desconhecido/pode receber qualquer valor. So pode ser atribuido em tipos unknown ou any 
+let nome3: unknown = 1
+// let num: number = nome3 // erro pois num e di tipo number
+let num: any = nome3
+console.log(nome3)
