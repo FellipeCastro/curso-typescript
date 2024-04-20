eda21dec1107c78073056699e3a384e7b6f297ab
Fellipe Castro <fellipecastro27d@gmail.com>
2024-04-20 14:16:57 -0300

    aula 09 finalizada

--- build/aula.js
+++ build/aula.js
@@ -1,13 +1,36 @@
 "use strict";
-let dados = {
-    nome: 'Fellipe',
-    idade: 16,
-    status: 'A',
-    ola: () => { console.log('Olá Mundo'); },
-    info: (nome) => { console.log(nome); }
-};
-console.log(typeof (dados));
-console.log(dados);
-console.log(dados.nome);
-dados.ola();
-dados.info(dados.nome);
+var dias;
+(function (dias) {
+    dias[dias["domingo"] = 0] = "domingo";
+    dias[dias["segunda"] = 1] = "segunda";
+    dias[dias["terca"] = 2] = "terca";
+    dias[dias["quarta"] = 3] = "quarta";
+    dias[dias["quinta"] = 4] = "quinta";
+    dias[dias["sexta"] = 5] = "sexta";
+    dias[dias["sabado"] = 6] = "sabado";
+})(dias || (dias = {}));
+console.log(dias.domingo);
+console.log(dias['segunda']);
+console.log(dias[3]);
+const dia = new Date();
+console.log(dia.getDay());
+console.log(dias[dia.getDay()]);
+var cores;
+(function (cores) {
+    cores["branco"] = "#fff";
+    cores["preto"] = "#000";
+    cores["vermelho"] = "#f00";
+    cores["verde"] = "#0f0";
+    cores["azul"] = "#00f";
+})(cores || (cores = {}));
+console.log(cores.branco);
+console.log(cores['verde']);
+var tipoUsuario;
+(function (tipoUsuario) {
+    tipoUsuario[tipoUsuario["user"] = 0] = "user";
+    tipoUsuario[tipoUsuario["admin"] = 1] = "admin";
+    tipoUsuario[tipoUsuario["super"] = 2] = "super";
+})(tipoUsuario || (tipoUsuario = {}));
+console.log(tipoUsuario.super);
+const tipo = 1;
+const tipo2 = tipoUsuario.admin;
--- src/aula.ts
+++ src/aula.ts
@@ -1,14 +1,43 @@
-let dados = {
-    nome: 'Fellipe',
-    idade: 16,
-    status: 'A',
-    ola: () => {console.log('Olá Mundo')},
-    info: (nome: string) => {console.log(nome)}
+// enum numerico
+enum dias {
+    domingo = 0,
+    segunda = 1,
+    terca = 2,
+    quarta = 3,
+    quinta = 4,
+    sexta = 5,
+    sabado = 6,
 }
 
-console.log(typeof(dados))
-console.log(dados)
-console.log(dados.nome)
+console.log(dias.domingo)
+console.log(dias['segunda'])
+console.log(dias[3])
 
-dados.ola()
-dados.info(dados.nome)
+const dia = new Date()
+console.log(dia.getDay())
+
+console.log(dias[dia.getDay()])
+
+// enum textual
+enum cores {
+    branco = '#fff',
+    preto = '#000',
+    vermelho = '#f00',
+    verde = '#0f0',
+    azul = '#00f',
+}
+
+console.log(cores.branco)
+console.log(cores['verde'])
+
+enum tipoUsuario {
+    user,
+    admin,
+    super
+}
+
+console.log(tipoUsuario.super)
+
+// usando enum como tipo
+const tipo: tipoUsuario = 1
+const tipo2: tipoUsuario = tipoUsuario.admin
