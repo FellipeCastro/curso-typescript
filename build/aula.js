88fbab331b866e449e0cb01e99f9dca673dcd718
Fellipe Castro <fellipecastro27d@gmail.com>
2024-04-24 18:06:00 -0300

    aula 20 finalizada

--- build/aula.js
+++ build/aula.js
@@ -1,52 +1,17 @@
 "use strict";
-class Computador {
-    nome;
-    ram;
-    cpu;
-    ligado;
-    constructor(nome, ram, cpu) {
-        this.nome = nome;
-        this.ram = ram;
-        this.cpu = cpu;
-        this.ligado = false;
-    }
-    info() {
-        console.log(`Nome   : ${this.nome}`);
-        console.log(`RAM    : ${this.ram}`);
-        console.log(`CPU    : ${this.cpu}`);
-        console.log(`Ligado : ${this.ligado ? 'Sim' : 'Não'}`);
-        console.log('');
-    }
-    ligar() {
-        this.ligado = true;
-    }
-    desligar() {
-        this.ligado = false;
-    }
-    upgradeRam(valor) {
-        if (valor >= 0 && valor <= 1000) {
-            this.ram = valor;
-        }
-        else {
-            console.log('Valor de memória RAM inválido');
-        }
-    }
-    upgradeCpu(valor) {
-        if (valor >= 0 && valor <= 1000) {
-            this.cpu = valor;
-        }
-        else {
-            console.log('Valor de CPU inválido');
-        }
+class Conta {
+    numero;
+    titular;
+    constructor(numero, titular) {
+        this.numero = numero;
+        this.titular = titular;
     }
 }
-const comp1 = new Computador('Computador 01', 8, 12);
-const comp2 = new Computador('Computador 02', 4, 6);
-const comp3 = new Computador('Computador 03', 16, 25);
-comp1.ligar();
-comp3.ligar();
-comp3.desligar();
-comp2.upgradeRam(64);
-comp1.info();
-comp2.info();
-comp3.info();
+class ContaPF extends Conta {
+}
+class ContaPJ extends Conta {
+}
+const conta01 = new ContaPF(123, 'Fellipe');
+const conta02 = new ContaPJ(456, 'João');
+console.log(conta01);
+console.log(conta02);
--- src/aula.ts
+++ src/aula.ts
@@ -1,62 +1,25 @@
-class Computador {
-    nome: string
-    private ram: number // propriedade privada/não pode ser modificada fora da classe
-    private cpu: number // propriedade privada/não pode ser modificada fora da classe
-    protected ligado: boolean // propriedade não pode ser modificada fora da classe, apenas por classes filhas
+// herança é uma classe que herda caracteristicas de outras 
 
-    // Método(função de uma classe) que será chamado sempre que a classe for instanciada
-    constructor(nome: string, ram: number, cpu: number) {
-        this.nome = nome // o this se refere a classe
-        this.ram = ram
-        this.cpu = cpu
-        this.ligado = false
-    }
+class Conta {
+    public numero: number
+    public titular: string
 
-    info(): void {
-        console.log(`Nome   : ${this.nome}`)
-        console.log(`RAM    : ${this.ram}`)
-        console.log(`CPU    : ${this.cpu}`)
-        console.log(`Ligado : ${this.ligado ? 'Sim' : 'Não'}`)
-        console.log('')
-    }
-
-    ligar(): void {
-        this.ligado = true
-    }
-
-    desligar(): void {
-        this.ligado = false
-    }
-
-    upgradeRam(valor: number): void {
-        if (valor >= 0 && valor <= 1000) {
-            this.ram = valor
-        } else {
-            console.log('Valor de memória RAM inválido')
-        }
-    }
-
-    upgradeCpu(valor: number): void {
-        if (valor >= 0 && valor <= 1000) {
-            this.cpu = valor
-        } else {
-            console.log('Valor de CPU inválido')
-        }
+    constructor(numero: number, titular: string) {
+        this.numero = numero
+        this.titular = titular
     }
 }
 
-// Instanciar/criar objeto da classe
-const comp1 = new Computador('Computador 01', 8, 12)
-const comp2 = new Computador('Computador 02', 4, 6)
-const comp3 = new Computador('Computador 03', 16, 25)
+class ContaPF extends Conta {
 
-comp1.ligar()
-comp3.ligar()
+}
 
-comp3.desligar()
+class ContaPJ extends Conta {
 
-comp2.upgradeRam(64)
+}
 
-comp1.info()
-comp2.info()
-comp3.info()
+const conta01 = new ContaPF(123, 'Fellipe')
+const conta02 = new ContaPJ(456, 'João')
+
+console.log(conta01)
+console.log(conta02)
