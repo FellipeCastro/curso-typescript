2d4d1238d325f07a946eb1138a4521e30061c1d3
Fellipe Castro <fellipecastro27d@gmail.com>
2024-05-06 11:10:52 -0300

    aula 36 finalizada

--- build/aula.js
+++ build/aula.js
@@ -7,7 +7,15 @@ const obj = {
     cor04: 'Vermelho',
 };
 let { cor01, cor02, cor03, cor04 } = obj;
-console.log(cor01);
-console.log(cor02);
-console.log(cor03);
-console.log(cor04);
+let [n1 = 0, n2 = 0, ...n3] = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const carros = () => {
+    return ['Uno', 'Gol', 'Fusca', 'Corsa'];
+};
+let [carro01, carro02, carro03, carro04] = carros();
+let texto = 'Curso de Typescript';
+let [...t] = texto.split(' ');
+let [p1, p2, p3] = texto.split(' ');
+console.log(t);
+console.log(p1);
+console.log(p2);
+console.log(p3);
